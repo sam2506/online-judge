685797e314603bdf876070dee82faa66f11f27ee
Samarth <samarth.kapoor@criodo.com>
2021-02-16 03:43:11 +0530

    added antMatchers for requests which require authentication in SecurityConfig

--- src/main/java/com/online/judge/config/security/SecurityConfig.java
+++ src/main/java/com/online/judge/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.online.judge.auth.services.JudgeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,9 +42,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/contests").authenticated()
+                .antMatchers(HttpMethod.POST, "/contests").authenticated()
+                .antMatchers(HttpMethod.POST, "/contests/{contestId}/problem/{problemId}").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/contests/{contestId}/problem/{problemId}").authenticated()
+                .antMatchers(HttpMethod.POST, "/contests/{contestId}/problem/{problemId}/submit").authenticated()
+                .antMatchers(HttpMethod.PUT, "/problems/{problemId}/edit").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/problems/{problemId}").authenticated()
+                .antMatchers(HttpMethod.POST, "/problems/{problemId}/uploadTestCases").authenticated()
+                .antMatchers(HttpMethod.POST, "/problems/{problemId}/submit").authenticated()
                 .anyRequest().permitAll()
-                .and()
-                .httpBasic();
+            .and()
+            .httpBasic();
     }
 }
